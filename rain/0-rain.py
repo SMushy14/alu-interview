6b5c0fca9aaffc8926b7f8216bc5fb7f46ff29e0
root <s.mushy1@alustudent.com>
2024-09-27 03:21:42 -0700

    module

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-
-def rain(walls):
-    """
+"""
 This module contains a function that calculates the amount of rainwater 
 trapped between walls after rainfall.
 
@@ -9,17 +7,35 @@ The rain() function takes in a list of wall heights and determines the
 total volume of water that can be trapped between the walls.
 """
 
+def rain(walls):
+    """
+    Calculate the total amount of rainwater trapped between walls after rainfall.
+
+    Given a list of non-negative integers where each integer represents 
+    the height of a wall with unit width 1, this function calculates 
+    how many square units of water can be trapped between the walls.
+
+    Parameters:
+    walls (list of int): A list of non-negative integers representing wall 
+    heights.
+    
+    Returns:
+    int: The total amount of rainwater trapped in square units.
+    
+    Example:
+    >>> rain([2, 0, 0, 4, 0, 0, 1, 0])
+    6
+    """
+    
     # If there are no walls, no water can be trapped
     if not walls:
         return 0
 
-    # Initialize pointers to the beginning (left) and end (right) of the walls
-    # list
+    # Initialize pointers to the beginning (left) and end (right) of the walls list
     left = 0
     right = len(walls) - 1
 
-    # These variables track the highest walls we've seen so far from the left
-    # and right
+    # These variables track the highest walls we've seen so far from the left and right
     left_max = 0
     right_max = 0
 
@@ -30,8 +46,7 @@ total volume of water that can be trapped between the walls.
     while left < right:
         # Compare the height of the left and right walls
         if walls[left] < walls[right]:
-            # If the current left wall is taller than the highest so far,
-            # update left_max
+            # If the current left wall is taller than the highest so far, update left_max
             if walls[left] >= left_max:
                 left_max = walls[left]
             # Otherwise, we can trap water at this position
@@ -40,8 +55,7 @@ total volume of water that can be trapped between the walls.
             # Move the left pointer to the right
             left += 1
         else:
-            # If the current right wall is taller than the highest so far,
-            # update right_max
+            # If the current right wall is taller than the highest so far, update right_max
             if walls[right] >= right_max:
                 right_max = walls[right]
             # Otherwise, we can trap water at this position
